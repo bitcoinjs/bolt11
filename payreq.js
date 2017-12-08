232c10cbdf43a8a77258a667f30c0a4df9b7d04b
junderw <junderwood@bitcoinbank.co.jp>
2017-12-08 22:46:08 +0900

    add encode wip

--- payreq.js
+++ payreq.js
@@ -7,6 +7,8 @@ const Buffer = require('safe-buffer').Buffer
 const BigNumber = require('bignumber.js')
 const bitcoinjs = require('bitcoinjs-lib')
 
+const VALIDWITNESSVERSIONS = [0]
+
 const BECH32CODES = {
   bc: 'bitcoin',
   tb: 'testnet'
@@ -22,6 +24,16 @@ const MULTIPLIERS = {
 const reduceWordsToIntBE = (total, item, index) => { return total + item * Math.pow(32, index) }
 
 const wordsToIntBE = (words) => words.reverse().reduce(reduceWordsToIntBE, 0)
+
+const intBEToWords = (intBE, bits) => {
+  let words = []
+  if (bits === undefined) bits = 5
+  while (intBE > 0) {
+    words.push(intBE & (Math.pow(2, bits) - 1))
+    intBE = intBE >> bits
+  }
+  return words.reverse()
+}
 
 const sha256 = (data) => crypto.createHash('sha256').update(data).digest()
 
@@ -61,6 +73,35 @@ const wordsTrimmedToBuffer = (words) => {
   return buffer
 }
 
+const hexToBuffer = (hex) => {
+  if (hex !== undefined
+      && (typeof hex === 'string' || hex instanceof String)
+      && hex.match(/^([a-zA-Z0-9]{2})*$/)) {
+    return Buffer.from(hex, 'hex')
+  }
+  return hex
+}
+
+const textToBuffer = (text) => {
+  if (text !== undefined
+      && (typeof text === 'string' || text instanceof String)) {
+    return Buffer.from(hex, 'utf8')
+  }
+  return text
+}
+
+const hexToWord = (hex) => {
+  let buffer = hexToBuffer(hex)
+  let words = convert(buffer, 8, 5, true)
+  return words
+}
+
+const textToWord = (text) => {
+  let buffer = textToBuffer(text)
+  let words = convert(buffer, 8, 5, true)
+  return words
+}
+
 const fallbackAddressParser = (words, network) => {
   let version = words[0]
   words = words.slice(1)
@@ -88,6 +129,8 @@ const fallbackAddressParser = (words, network) => {
   }
 }
 
+const fallbackAddressEncoder = (data) => [data.code].concat(hexToWord(data.addressHash))
+
 const routingInfoParser = (words) => {
   let routes = []
   let pubkey, short_channel_id, fee_mSats, cltv_expiry_delta
@@ -110,6 +153,17 @@ const routingInfoParser = (words) => {
   return routes
 }
 
+const routingInfoEncoder = (datas) => {
+  let buffer = Buffer(0)
+  datas.forEach(data => {
+    buffer = Buffer.concat([buffer, hexToBuffer(data.pubkey)])
+    buffer = Buffer.concat([buffer, hexToBuffer(data.short_channel_id)])
+    buffer = Buffer.concat([buffer, Buffer(intBEToWords(data.fee_mSats, 8))])
+    buffer = Buffer.concat([buffer, Buffer(intBEToWords(data.cltv_expiry_delta, 8))])
+  })
+  return hexToWord(buffer)
+}
+
 const TAGNAMES = {
   '1': 'payment_hash',
   '13': 'description',
@@ -119,6 +173,28 @@ const TAGNAMES = {
   '24': 'min_final_cltv_expiry', // default: 9
   '9': 'fallback_address',
   '3': 'routing_info' // for extra routing info (private etc.)
+}
+
+const TAGCODES = {
+  'payment_hash': 1,
+  'description': 13,
+  'payee_node_key': 19,
+  'purpose_commit_hash': 23, // commit to longer descriptions (like a website)
+  'expire_time': 6, // default: 3600 (1 hour)
+  'min_final_cltv_expiry': 24, // default: 9
+  'fallback_address': 9,
+  'routing_info': 3 // for extra routing info (private etc.)
+}
+
+const TAGENCODERS = {
+  'payment_hash': hexToWord, // 256 bits
+  'description': textToWord, // string variable length
+  'payee_node_key': hexToWord, // 264 bits
+  'purpose_commit_hash': hexToWord, // 256 bits
+  'expire_time': intBEToWords, // default: 3600 (1 hour)
+  'min_final_cltv_expiry': intBEToWords, // default: 9
+  'fallback_address': fallbackAddressEncoder,
+  'routing_info': routingInfoEncoder // for extra routing info (private etc.)
 }
 
 const TAGPARSERS = {
@@ -132,11 +208,172 @@ const TAGPARSERS = {
   '3': routingInfoParser // for extra routing info (private etc.)
 }
 
-function encode (dataObj) {
+/* MUST but default OK:
+  coinType  (default: testnet OK)
+  timestamp   (default: current time OK)
 
+  MUST:
+  signature OR privatekey
+  tags[TAGNAMES['1']] (payment hash)
+  tags[TAGNAMES['13']] OR tags[TAGNAMES['23']] (description or description for hashing (or description hash))
+
+  MUST CHECK:
+  IF tags[TAGNAMES['19']] THEN MUST CHECK THAT PUBKEY = PUBKEY OF PRIVATEKEY / SIGNATURE
+  IF tags[TAGNAMES['9']] THEN MUST CHECK THAT THE ADDRESS IS A VALID TYPE
+  IF tags[TAGNAMES['3']] THEN MUST CHECK FOR ALL INFO IN EACH
+*/
+const encode  = (data) => {
+  if (data.coinType === undefined) data.coinType = bitcoinjs.networks.testnet
+  if (data.timestamp === undefined) data.timestamp = Math.floor(new Date().getTime() / 1000)
+  if (data.tags[TAGNAMES['1']] === undefined) {
+    throw new Error('Lightning Payment Request needs a payment hash')
+  }
+  if (data.tags[TAGNAMES['13']] === undefined && data.tags[TAGNAMES['23']] === undefined) {
+    throw new Error('Lightning Payment Request needs a description or a purpose commit hash (or message)')
+  }
+  if (data.privateKey === undefined) {
+    throw new Error('Lightning Payment Request needs signature OR privateKey buffer')
+  }
+
+  let privateKey, publicKey, nodePublicKey
+  if (data.tags[TAGNAMES['19']]) nodePublicKey = hexToBuffer(data.tags[TAGNAMES['19']])
+  if (data.privateKey) {
+    privateKey = hexToBuffer(data.privateKey)
+    if (privateKey.length !== 32 || !secp256k1.privateKeyVerify(privateKey)) {
+      throw new Error('The private key given is not valid for SECP256K1')
+    }
+    // Check if pubkey matches for private key here.
+    // For signature we must wait until all the info is ready for writing.
+    if (data.tags[TAGNAMES['19']]) {
+      publicKey = secp256k1.publicKeyCreate(privateKey)
+      if (!publicKey.equals(nodePublicKey)) {
+        throw new Error('The private key given is not the private key of the node public key given')
+      }
+    }
+  }
+
+  let code, addressHash, address
+  if (data.tags[TAGNAMES['9']]) {
+    address = data.tags[TAGNAMES['9']].address
+
+    try {
+      let bech32addr = bitcoinjs.address.fromBech32(address)
+      if (!(bech32addr.version in VALIDWITNESSVERSIONS)) {
+        throw new Error('Fallback address witness version is unknown')
+      }
+      if (bech32addr.prefix !== data.coinType.bech32) {
+        throw new Error('Fallback address network type does not match payment request network type')
+      }
+      addressHash = bech32addr.data
+      code = bech32addr.version
+    } catch () {
+      try {
+        let base58addr = bitcoinjs.address.fromBase58Check(address)
+        if (base58addr.version === data.coinType.pubKeyHash) {
+          code = 17
+        } else if (base58addr.version === data.coinType.scriptHash) {
+          code = 18
+        } else {
+          throw new Error('Fallback address version (base58) is unknown or the network type is incorrect')
+        }
+        addressHash = base58addr.hash
+      } catch () {
+        throw new Error('Fallback address type is unknown')
+      }
+    }
+  }
+
+  if (data.tags[TAGNAMES['3']]) {
+    let routing_info = data.tags[TAGNAMES['3']]
+    routing_info.forEach(route => {
+      if (route.pubkey === undefined
+        || route.short_channel_id === undefined
+        || route.fee_mSats === undefined
+        || route.cltv_expiry_delta === undefined) {
+          throw new Error('Routing info is incomplete')
+      }
+      if (!secp256k1.publicKeyVerify(hexToBuffer(route.pubkey))) {
+        throw new Error('Routing info pubkey is not a valid pubkey')
+      }
+      let shortId = hexToBuffer(route.short_channel_id)
+      if (!(shortId instanceof Buffer) || shortId.length !== 8) {
+        throw new Error('Routing info short channel id must be 8 bytes')
+      }
+      if (typeof route.fee_mSats !== 'number'
+        || Math.floor(route.fee_mSats) !== route.fee_mSats) {
+          throw new Error('Routing info fee is not an integer')
+      }
+      if (typeof route.cltv_expiry_delta !== 'number'
+        || Math.floor(route.cltv_expiry_delta) !== route.cltv_expiry_delta) {
+          throw new Error('Routing info cltv expiry delta is not an integer')
+      }
+    })
+  }
+
+  let prefix = 'ln'
+  prefix += data.coinType.bech32
+
+  if (data.satoshis) {
+    let mSats = BigNumber(1000).mul(data.satoshis)
+    let mSatsString = mSats.toString(10).replace(/\.\d*$/,'')
+    let mSatsLength = mSatsString.length
+    let multiplier, value
+    if (mSatsLength > 11 && mSatsString.slice(-11) === '00000000000') {
+      multiplier = ''
+      value = mSats.div(1e11).toString(10)
+    } else if (mSatsLength > 8 && mSatsString.slice(-8) === '00000000') {
+      multiplier = 'm'
+      value = mSats.div(1e8).toString(10)
+    } else if (mSatsLength > 5 && mSatsString.slice(-5) === '00000') {
+      multiplier = 'u'
+      value = mSats.div(1e5).toString(10)
+    } else if (mSatsLength > 2 && mSatsString.slice(-2) === '00') {
+      multiplier = 'n'
+      value = mSats.div(1e2).toString(10)
+    } else {
+      multiplier = 'p'
+      value = mSats.mul(10).toString(10)
+    }
+  } else {
+    multiplier = ''
+    value = ''
+  }
+
+  prefix += value + multiplier
+
+  let timestampHex = data.timestamp.toString(16)
+  if (timestampHex.length < 8) timestampHex = ('00000000' + timestampHex).slice(-8)
+  let timestampBuffer = Buffer.from(timestampHex, 'hex')
+  let timestampWords = convert(timestampBuffer, 8, 5, true)
+
+  let tags = data.tags
+  let tagKeys = Object.keys(tags)
+  let tagWords = []
+  tagKeys.forEach(tagKey => {
+    if (!(tagKey in Object.keys(TAGENCODERS))) {
+      throw new Error('Unknown tag key: ' + tagKey)
+    }
+    tagWords.push(TAGCODES[tagKey])
+    let encoder = TAGENCODERS[tagKey]
+    let words = encoder(tags[tagKey])
+    tagWords = tagWords.concat(intBEToWords(words.length))
+    tagWords = tagWords.concat(words)
+  })
+
+  let dataWords = timestampWords.concat(tagWords)
+
+  let toSign = Buffer.concat([Buffer.from(prefix, 'utf8'), Buffer.from(convert(dataWords, 5, 8, true))])
+  let payReqHash = sha256(toSign)
+
+  let sigObj = secp256k1.sign(payReqHash, privateKey)
+  let sigWords = hexToWord(sigObj.signature.toString('hex') + '0' + sigObj.recovery)
+
+  dataWords = dataWords.concat(sigWords)
+
+  return bech32.encode(prefix, dataWords, 999999999)
 }
 
-function decode (paymentRequest) {
+const decode = (paymentRequest) => {
   if (paymentRequest.slice(0,2) !== 'ln') throw new Error('Not a proper lightning payment request')
   let { prefix, words } = bech32.decode(paymentRequest, 1023)
 
@@ -152,7 +389,8 @@ function decode (paymentRequest) {
     throw new Error('Signature is missing or incorrect')
   }
 
-  let prefixMatches = prefix.match(/^ln(\S*?)(\d*)([munp]?)$/)
+  let prefixMatches = prefix.match(/^ln(\S*?)(\d*)([a-zA-Z]?)$/)
+  if (!prefixMatches) throw new Error('Not a proper lightning payment request')
 
   let coinType = prefixMatches[1]
   let coinNetwork = bitcoinjs.networks['bitcoin']
@@ -166,6 +404,7 @@ function decode (paymentRequest) {
   if (value) {
     let valueInt = parseInt(value)
     let multiplier = prefixMatches[3]
+    if (!multiplier.match(/^[munp]$/)) throw new Error('Unknown multiplier used in amount')
     satoshis = multiplier ? MULTIPLIERS[multiplier].mul(valueInt).mul(1e8).toNumber() : valueInt * 1e8
   } else {
     satoshis = null
