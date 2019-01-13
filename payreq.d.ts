8ce248f3c2cd657cbc5b2da9740c62d3c01dee34
junderw <junderwood@bitcoinbank.co.jp>
2019-01-13 19:44:54 +0900

    Add tag data types

--- payreq.d.ts
+++ payreq.d.ts
@@ -1,5 +1,21 @@
 import * as BN from "bn.js";
-type PaymentRequestObject = {
+
+type RoutingInfo = Array<{
+  pubkey: string;
+  short_channel_id: string;
+  fee_base_msat: number;
+  fee_proportional_millionths: number;
+  cltv_expiry_delta: number;
+}>;
+type FallbackAddress = {
+  code: number;
+  address: string;
+  addressHash: string;
+};
+
+// Start exports
+export declare type TagData = string | number | RoutingInfo | FallbackAddress;
+export declare type PaymentRequestObject = {
   paymentRequest?: string;
   complete?: boolean;
   prefix?: string;
@@ -16,14 +32,13 @@ type PaymentRequestObject = {
   recoveryFlag?: number;
   tags: Array<{
     tagName: string;
-    data: any;
+    data: TagData;
   }>;
 };
-declare function encode(inputData: PaymentRequestObject, addDefaults?: boolean): PaymentRequestObject;
-declare function decode(paymentRequest: string): PaymentRequestObject;
-declare function sign(inputPayReqObj: PaymentRequestObject, inputPrivateKey: string | Buffer): PaymentRequestObject;
-declare function satToHrp(satoshis: number | string): string;
-declare function millisatToHrp(millisatoshis: number | string): string;
-declare function hrpToSat(hrpString: string, outputString?: boolean): string | BN;
-declare function hrpToMillisat(hrpString: string, outputString?: boolean): string | BN;
-export { encode, decode, sign, satToHrp, millisatToHrp, hrpToSat, hrpToMillisat };
+export declare function encode(inputData: PaymentRequestObject, addDefaults?: boolean): PaymentRequestObject;
+export declare function decode(paymentRequest: string): PaymentRequestObject;
+export declare function sign(inputPayReqObj: PaymentRequestObject, inputPrivateKey: string | Buffer): PaymentRequestObject;
+export declare function satToHrp(satoshis: number | string): string;
+export declare function millisatToHrp(millisatoshis: number | string): string;
+export declare function hrpToSat(hrpString: string, outputString?: boolean): string | BN;
+export declare function hrpToMillisat(hrpString: string, outputString?: boolean): string | BN;
