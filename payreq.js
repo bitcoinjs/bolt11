f8993297ac0b8c5db6f12fa726694a40e194b065
junderw <junderwood@bitcoinbank.co.jp>
2020-03-19 08:11:47 +0900

    Fix dependencies for vuln and standard rules

--- payreq.js
+++ payreq.js
@@ -61,8 +61,8 @@ const TAGCODES = {
 // reverse the keys and values of TAGCODES and insert into TAGNAMES
 const TAGNAMES = {}
 for (let i = 0, keys = Object.keys(TAGCODES); i < keys.length; i++) {
-  let currentName = keys[i]
-  let currentCode = TAGCODES[keys[i]].toString()
+  const currentName = keys[i]
+  const currentCode = TAGCODES[keys[i]].toString()
   TAGNAMES[currentCode] = currentName
 }
 
@@ -78,14 +78,14 @@ const TAGENCODERS = {
 }
 
 const TAGPARSERS = {
-  '1': (words) => wordsToBuffer(words, true).toString('hex'), // 256 bits
-  '13': (words) => wordsToBuffer(words, true).toString('utf8'), // string variable length
-  '19': (words) => wordsToBuffer(words, true).toString('hex'), // 264 bits
-  '23': (words) => wordsToBuffer(words, true).toString('hex'), // 256 bits
-  '6': wordsToIntBE, // default: 3600 (1 hour)
-  '24': wordsToIntBE, // default: 9
-  '9': fallbackAddressParser,
-  '3': routingInfoParser // for extra routing info (private etc.)
+  1: (words) => wordsToBuffer(words, true).toString('hex'), // 256 bits
+  13: (words) => wordsToBuffer(words, true).toString('utf8'), // string variable length
+  19: (words) => wordsToBuffer(words, true).toString('hex'), // 264 bits
+  23: (words) => wordsToBuffer(words, true).toString('hex'), // 256 bits
+  6: wordsToIntBE, // default: 3600 (1 hour)
+  24: wordsToIntBE, // default: 9
+  9: fallbackAddressParser,
+  3: routingInfoParser // for extra routing info (private etc.)
 }
 
 const unknownTagName = 'unknownTag'
@@ -109,7 +109,7 @@ function wordsToIntBE (words) {
 }
 
 function intBEToWords (intBE, bits) {
-  let words = []
+  const words = []
   if (bits === undefined) bits = 5
   intBE = Math.floor(intBE)
   if (intBE === 0) return [0]
@@ -127,9 +127,9 @@ function sha256 (data) {
 function convert (data, inBits, outBits) {
   let value = 0
   let bits = 0
-  let maxV = (1 << outBits) - 1
+  const maxV = (1 << outBits) - 1
 
-  let result = []
+  const result = []
   for (let i = 0; i < data.length; ++i) {
     value = (value << inBits) | data[i]
     bits += inBits
@@ -169,22 +169,22 @@ function textToBuffer (text) {
 }
 
 function hexToWord (hex) {
-  let buffer = hexToBuffer(hex)
+  const buffer = hexToBuffer(hex)
   return bech32.toWords(buffer)
 }
 
 function textToWord (text) {
-  let buffer = textToBuffer(text)
-  let words = bech32.toWords(buffer)
+  const buffer = textToBuffer(text)
+  const words = bech32.toWords(buffer)
   return words
 }
 
 // see encoder for details
 function fallbackAddressParser (words, network) {
-  let version = words[0]
+  const version = words[0]
   words = words.slice(1)
 
-  let addressHash = wordsToBuffer(words, true)
+  const addressHash = wordsToBuffer(words, true)
 
   let address = null
 
@@ -218,7 +218,7 @@ function fallbackAddressEncoder (data, network) {
 // first convert from words to buffer, trimming padding where necessary
 // parse in 51 byte chunks. See encoder for details.
 function routingInfoParser (words) {
-  let routes = []
+  const routes = []
   let pubkey, shortChannelId, feeBaseMSats, feeProportionalMillionths, cltvExpiryDelta
   let routesBuffer = wordsToBuffer(words, true)
   while (routesBuffer.length > 0) {
@@ -276,8 +276,8 @@ function purposeCommitEncoder (data) {
 }
 
 function tagsItems (tags, tagName) {
-  let tag = tags.filter(item => item.tagName === tagName)
-  let data = tag.length > 0 ? tag[0].data : null
+  const tag = tags.filter(item => item.tagName === tagName)
+  const data = tag.length > 0 ? tag[0].data : null
   return data
 }
 
@@ -286,7 +286,7 @@ function tagsContainItem (tags, tagName) {
 }
 
 function orderKeys (unorderedObj) {
-  let orderedObj = {}
+  const orderedObj = {}
   Object.keys(unorderedObj).sort().forEach((key) => {
     orderedObj[key] = unorderedObj[key]
   })
@@ -297,7 +297,7 @@ function satToHrp (satoshis) {
   if (!satoshis.toString().match(/^\d+$/)) {
     throw new Error('satoshis must be an integer')
   }
-  let millisatoshisBN = new BN(satoshis, 10)
+  const millisatoshisBN = new BN(satoshis, 10)
   return millisatToHrp(millisatoshisBN.mul(new BN(1000, 10)))
 }
 
@@ -305,9 +305,9 @@ function millisatToHrp (millisatoshis) {
   if (!millisatoshis.toString().match(/^\d+$/)) {
     throw new Error('millisatoshis must be an integer')
   }
-  let millisatoshisBN = new BN(millisatoshis, 10)
-  let millisatoshisString = millisatoshisBN.toString(10)
-  let millisatoshisLength = millisatoshisString.length
+  const millisatoshisBN = new BN(millisatoshis, 10)
+  const millisatoshisString = millisatoshisBN.toString(10)
+  const millisatoshisLength = millisatoshisString.length
   let divisorString, valueString
   if (millisatoshisLength > 11 && /0{11}$/.test(millisatoshisString)) {
     divisorString = ''
@@ -329,11 +329,11 @@ function millisatToHrp (millisatoshis) {
 }
 
 function hrpToSat (hrpString, outputString) {
-  let millisatoshisBN = hrpToMillisat(hrpString, false)
+  const millisatoshisBN = hrpToMillisat(hrpString, false)
   if (!millisatoshisBN.mod(new BN(1000, 10)).eq(new BN(0, 10))) {
     throw new Error('Amount is outside of valid range')
   }
-  let result = millisatoshisBN.div(new BN(1000, 10))
+  const result = millisatoshisBN.div(new BN(1000, 10))
   return outputString ? result.toString() : result
 }
 
@@ -350,9 +350,9 @@ function hrpToMillisat (hrpString, outputString) {
 
   if (!value.match(/^\d+$/)) throw new Error('Not a valid human readable amount')
 
-  let valueBN = new BN(value, 10)
+  const valueBN = new BN(value, 10)
 
-  let millisatoshisBN = divisor
+  const millisatoshisBN = divisor
     ? valueBN.mul(MILLISATS_PER_BTC).div(DIVISORS[divisor])
     : valueBN.mul(MILLISATS_PER_BTC)
 
@@ -365,8 +365,8 @@ function hrpToMillisat (hrpString, outputString) {
 }
 
 function sign (inputPayReqObj, inputPrivateKey) {
-  let payReqObj = cloneDeep(inputPayReqObj)
-  let privateKey = hexToBuffer(inputPrivateKey)
+  const payReqObj = cloneDeep(inputPayReqObj)
+  const privateKey = hexToBuffer(inputPrivateKey)
   if (payReqObj.complete && payReqObj.paymentRequest) return payReqObj
 
   if (privateKey === undefined || privateKey.length !== 32 ||
@@ -391,27 +391,27 @@ function sign (inputPayReqObj, inputPrivateKey) {
   // make sure if either exist they are in nodePublicKey
   nodePublicKey = tagNodePublicKey || nodePublicKey
 
-  let publicKey = secp256k1.publicKeyCreate(privateKey)
+  const publicKey = secp256k1.publicKeyCreate(privateKey)
 
   // Check if pubkey matches for private key
   if (nodePublicKey && !publicKey.equals(nodePublicKey)) {
     throw new Error('The private key given is not the private key of the node public key given')
   }
 
-  let words = bech32.decode(payReqObj.wordsTemp, Number.MAX_SAFE_INTEGER).words
+  const words = bech32.decode(payReqObj.wordsTemp, Number.MAX_SAFE_INTEGER).words
 
   // the preimage for the signing data is the buffer of the prefix concatenated
   // with the buffer conversion of the data words excluding the signature
   // (right padded with 0 bits)
-  let toSign = Buffer.concat([Buffer.from(payReqObj.prefix, 'utf8'), wordsToBuffer(words)])
+  const toSign = Buffer.concat([Buffer.from(payReqObj.prefix, 'utf8'), wordsToBuffer(words)])
   // single SHA256 hash for the signature
-  let payReqHash = sha256(toSign)
+  const payReqHash = sha256(toSign)
 
   // signature is 64 bytes (32 byte r value and 32 byte s value concatenated)
   // PLUS one extra byte appended to the right with the recoveryID in [0,1,2,3]
   // Then convert to 5 bit words with right padding 0 bits.
-  let sigObj = secp256k1.sign(payReqHash, privateKey)
-  let sigWords = hexToWord(sigObj.signature.toString('hex') + '0' + sigObj.recovery)
+  const sigObj = secp256k1.sign(payReqHash, privateKey)
+  const sigWords = hexToWord(sigObj.signature.toString('hex') + '0' + sigObj.recovery)
 
   // append signature words to the words, mark as complete, and add the payreq
   payReqObj.payeeNodeKey = publicKey.toString('hex')
@@ -440,12 +440,12 @@ function sign (inputPayReqObj, inputPrivateKey) {
 */
 function encode (inputData, addDefaults) {
   // we don't want to affect the data being passed in, so we copy the object
-  let data = cloneDeep(inputData)
+  const data = cloneDeep(inputData)
 
   // by default we will add default values to description, expire time, and min cltv
   if (addDefaults === undefined) addDefaults = true
 
-  let canReconstruct = !(data.signature === undefined || data.recoveryFlag === undefined)
+  const canReconstruct = !(data.signature === undefined || data.recoveryFlag === undefined)
 
   // if no cointype is defined, set to testnet
   let coinTypeObj
@@ -530,7 +530,7 @@ function encode (inputData, addDefaults) {
   let code, addressHash, address
   // If there is a fallback address tag we must check it is valid
   if (tagsContainItem(data.tags, TAGNAMES['9'])) {
-    let addrData = tagsItems(data.tags, TAGNAMES['9'])
+    const addrData = tagsItems(data.tags, TAGNAMES['9'])
     // Most people will just provide address so Hash and code will be undefined here
     address = addrData.address
     addressHash = addrData.addressHash
@@ -577,7 +577,7 @@ function encode (inputData, addDefaults) {
 
   // If there is route info tag, check that each route has all 4 necessary info
   if (tagsContainItem(data.tags, TAGNAMES['3'])) {
-    let routingInfo = tagsItems(data.tags, TAGNAMES['3'])
+    const routingInfo = tagsItems(data.tags, TAGNAMES['3'])
     routingInfo.forEach(route => {
       if (route.pubkey === undefined ||
         route.short_channel_id === undefined ||
@@ -589,7 +589,7 @@ function encode (inputData, addDefaults) {
       if (!secp256k1.publicKeyVerify(hexToBuffer(route.pubkey))) {
         throw new Error('Routing info pubkey is not a valid pubkey')
       }
-      let shortId = hexToBuffer(route.short_channel_id)
+      const shortId = hexToBuffer(route.short_channel_id)
       if (!(shortId instanceof Buffer) || shortId.length !== 8) {
         throw new Error('Routing info short channel id must be 8 bytes')
       }
@@ -616,7 +616,7 @@ function encode (inputData, addDefaults) {
   // divisor (m = milli, u = micro, n = nano, p = pico)
   if (data.millisatoshis && data.satoshis) {
     hrpString = millisatToHrp(new BN(data.millisatoshis, 10))
-    let hrpStringSat = satToHrp(new BN(data.satoshis, 10))
+    const hrpStringSat = satToHrp(new BN(data.satoshis, 10))
     if (hrpStringSat !== hrpString) {
       throw new Error('satoshis and millisatoshis do not match')
     }
@@ -633,9 +633,9 @@ function encode (inputData, addDefaults) {
   prefix += hrpString
 
   // timestamp converted to 5 bit number array (left padded with 0 bits, NOT right padded)
-  let timestampWords = intBEToWords(data.timestamp)
+  const timestampWords = intBEToWords(data.timestamp)
 
-  let tags = data.tags
+  const tags = data.tags
   let tagWords = []
   tags.forEach(tag => {
     const possibleTagNames = Object.keys(TAGENCODERS)
@@ -653,7 +653,7 @@ function encode (inputData, addDefaults) {
       const encoder = TAGENCODERS[tag.tagName]
       words = encoder(tag.data)
     } else {
-      let result = unknownEncoder(tag.data)
+      const result = unknownEncoder(tag.data)
       tagWords.push(result.tagCode)
       words = result.words
     }
@@ -671,9 +671,9 @@ function encode (inputData, addDefaults) {
   // the preimage for the signing data is the buffer of the prefix concatenated
   // with the buffer conversion of the data words excluding the signature
   // (right padded with 0 bits)
-  let toSign = Buffer.concat([Buffer.from(prefix, 'utf8'), Buffer.from(convert(dataWords, 5, 8))])
+  const toSign = Buffer.concat([Buffer.from(prefix, 'utf8'), Buffer.from(convert(dataWords, 5, 8))])
   // single SHA256 hash for the signature
-  let payReqHash = sha256(toSign)
+  const payReqHash = sha256(toSign)
 
   // signature is 64 bytes (32 byte r value and 32 byte s value concatenated)
   // PLUS one extra byte appended to the right with the recoveryID in [0,1,2,3]
@@ -689,7 +689,7 @@ function encode (inputData, addDefaults) {
     Earlier we check if the private key matches the payee node key IF they
     gave one. */
     if (nodePublicKey) {
-      let recoveredPubkey = secp256k1.recover(payReqHash, Buffer.from(data.signature, 'hex'), data.recoveryFlag, true)
+      const recoveredPubkey = secp256k1.recover(payReqHash, Buffer.from(data.signature, 'hex'), data.recoveryFlag, true)
       if (nodePublicKey && !nodePublicKey.equals(recoveredPubkey)) {
         throw new Error('Signature, message, and recoveryID did not produce the same pubkey as payeeNodeKey')
       }
@@ -721,21 +721,21 @@ function encode (inputData, addDefaults) {
 function decode (paymentRequest, network) {
   if (typeof paymentRequest !== 'string') throw new Error('Lightning Payment Request must be string')
   if (paymentRequest.slice(0, 2).toLowerCase() !== 'ln') throw new Error('Not a proper lightning payment request')
-  let decoded = bech32.decode(paymentRequest, Number.MAX_SAFE_INTEGER)
+  const decoded = bech32.decode(paymentRequest, Number.MAX_SAFE_INTEGER)
   paymentRequest = paymentRequest.toLowerCase()
-  let prefix = decoded.prefix
+  const prefix = decoded.prefix
   let words = decoded.words
 
   // signature is always 104 words on the end
   // cutting off at the beginning helps since there's no way to tell
   // ahead of time how many tags there are.
-  let sigWords = words.slice(-104)
+  const sigWords = words.slice(-104)
   // grabbing a copy of the words for later, words will be sliced as we parse.
-  let wordsNoSig = words.slice(0, -104)
+  const wordsNoSig = words.slice(0, -104)
   words = words.slice(0, -104)
 
   let sigBuffer = wordsToBuffer(sigWords, true)
-  let recoveryFlag = sigBuffer.slice(-1)[0]
+  const recoveryFlag = sigBuffer.slice(-1)[0]
   sigBuffer = sigBuffer.slice(0, -1)
 
   if (!(recoveryFlag in [0, 1, 2, 3]) || sigBuffer.length !== 64) {
@@ -753,7 +753,7 @@ function decode (paymentRequest, network) {
     throw new Error('Not a proper lightning payment request')
   }
 
-  let bech32Prefix = prefixMatches[1]
+  const bech32Prefix = prefixMatches[1]
   let coinNetwork
   if (!network) {
     switch (bech32Prefix) {
@@ -780,10 +780,10 @@ function decode (paymentRequest, network) {
     throw new Error('Unknown coin bech32 prefix')
   }
 
-  let value = prefixMatches[2]
+  const value = prefixMatches[2]
   let satoshis, millisatoshis, removeSatoshis
   if (value) {
-    let divisor = prefixMatches[3]
+    const divisor = prefixMatches[3]
     try {
       satoshis = parseInt(hrpToSat(value + divisor, true))
     } catch (e) {
@@ -797,16 +797,16 @@ function decode (paymentRequest, network) {
   }
 
   // reminder: left padded 0 bits
-  let timestamp = wordsToIntBE(words.slice(0, 7))
-  let timestampString = new Date(timestamp * 1000).toISOString()
+  const timestamp = wordsToIntBE(words.slice(0, 7))
+  const timestampString = new Date(timestamp * 1000).toISOString()
   words = words.slice(7) // trim off the left 7 words
 
-  let tags = []
+  const tags = []
   let tagName, parser, tagLength, tagWords
   // we have no tag count to go on, so just keep hacking off words
   // until we have none.
   while (words.length > 0) {
-    let tagCode = words[0].toString()
+    const tagCode = words[0].toString()
     tagName = TAGNAMES[tagCode] || unknownTagName
     parser = TAGPARSERS[tagCode] || getUnknownParser(tagCode)
     words = words.slice(1)
@@ -832,9 +832,9 @@ function decode (paymentRequest, network) {
     timeExpireDateString = new Date(timeExpireDate * 1000).toISOString()
   }
 
-  let toSign = Buffer.concat([Buffer.from(prefix, 'utf8'), Buffer.from(convert(wordsNoSig, 5, 8))])
-  let payReqHash = sha256(toSign)
-  let sigPubkey = secp256k1.recover(payReqHash, sigBuffer, recoveryFlag, true)
+  const toSign = Buffer.concat([Buffer.from(prefix, 'utf8'), Buffer.from(convert(wordsNoSig, 5, 8))])
+  const payReqHash = sha256(toSign)
+  const sigPubkey = secp256k1.recover(payReqHash, sigBuffer, recoveryFlag, true)
   if (tagsContainItem(tags, TAGNAMES['19']) && tagsItems(tags, TAGNAMES['19']) !== sigPubkey.toString('hex')) {
     throw new Error('Lightning Payment Request signature pubkey does not match payee pubkey')
   }
@@ -856,11 +856,11 @@ function decode (paymentRequest, network) {
   }
 
   if (removeSatoshis) {
-    delete finalResult['satoshis']
+    delete finalResult.satoshis
   }
 
   if (timeExpireDate) {
-    finalResult = Object.assign(finalResult, {timeExpireDate, timeExpireDateString})
+    finalResult = Object.assign(finalResult, { timeExpireDate, timeExpireDateString })
   }
 
   return orderKeys(finalResult)
--- test/index.js
+++ test/index.js
@@ -1,25 +1,25 @@
 'use strict'
-let tape = require('tape')
-let fixtures = require('./fixtures')
-let lnpayreq = require('../')
-let BN = require('bn.js')
+const tape = require('tape')
+const fixtures = require('./fixtures')
+const lnpayreq = require('../')
+const BN = require('bn.js')
 
 fixtures.satToHrp.valid.forEach((f) => {
-  tape(`test valid satoshi to hrp string`, (t) => {
+  tape('test valid satoshi to hrp string', (t) => {
     t.plan(1)
     t.same(f.output, lnpayreq.satToHrp(new BN(f.input, 10)))
   })
 })
 
 fixtures.millisatToHrp.valid.forEach((f) => {
-  tape(`test valid millisatoshi to hrp string`, (t) => {
+  tape('test valid millisatoshi to hrp string', (t) => {
     t.plan(1)
     t.same(f.output, lnpayreq.millisatToHrp(new BN(f.input, 10)))
   })
 })
 
 fixtures.satToHrp.invalid.forEach((f) => {
-  tape(`test invalid satoshi to hrp string`, (t) => {
+  tape('test invalid satoshi to hrp string', (t) => {
     t.plan(1)
     t.throws(() => {
       lnpayreq.satToHrp(f.input)
@@ -28,7 +28,7 @@ fixtures.satToHrp.invalid.forEach((f) => {
 })
 
 fixtures.millisatToHrp.invalid.forEach((f) => {
-  tape(`test invalid millisatoshi to hrp string`, (t) => {
+  tape('test invalid millisatoshi to hrp string', (t) => {
     t.plan(1)
     t.throws(() => {
       lnpayreq.millisatToHrp(f.input)
@@ -37,21 +37,21 @@ fixtures.millisatToHrp.invalid.forEach((f) => {
 })
 
 fixtures.hrpToSat.valid.forEach((f) => {
-  tape(`test valid hrp string to satoshi`, (t) => {
+  tape('test valid hrp string to satoshi', (t) => {
     t.plan(1)
     t.same(f.output, lnpayreq.hrpToSat(f.input).toString())
   })
 })
 
 fixtures.hrpToMillisat.valid.forEach((f) => {
-  tape(`test valid hrp string to millisatoshi`, (t) => {
+  tape('test valid hrp string to millisatoshi', (t) => {
     t.plan(1)
     t.same(f.output, lnpayreq.hrpToMillisat(f.input).toString())
   })
 })
 
 fixtures.hrpToSat.invalid.forEach((f) => {
-  tape(`test invalid hrp string to satoshi`, (t) => {
+  tape('test invalid hrp string to satoshi', (t) => {
     t.plan(1)
     t.throws(() => {
       lnpayreq.hrpToSat(f.input)
@@ -60,7 +60,7 @@ fixtures.hrpToSat.invalid.forEach((f) => {
 })
 
 fixtures.hrpToMillisat.invalid.forEach((f) => {
-  tape(`test invalid hrp string to millisatoshi`, (t) => {
+  tape('test invalid hrp string to millisatoshi', (t) => {
     t.plan(1)
     t.throws(() => {
       lnpayreq.hrpToMillisat(f.input)
@@ -69,10 +69,10 @@ fixtures.hrpToMillisat.invalid.forEach((f) => {
 })
 
 fixtures.sign.invalid.forEach((f) => {
-  tape(`test invalid vectors for sign`, (t) => {
+  tape('test invalid vectors for sign', (t) => {
     t.plan(1)
 
-    let privateKey = f.privateKey
+    const privateKey = f.privateKey
       ? Buffer.from(f.privateKey, 'hex')
       : Buffer.from(fixtures.privateKey, 'hex')
 
@@ -83,10 +83,10 @@ fixtures.sign.invalid.forEach((f) => {
 })
 
 fixtures.encode.valid.forEach((f) => {
-  tape(`test valid vectors for encode`, (t) => {
-    let encoded = lnpayreq.encode(f.data)
+  tape('test valid vectors for encode', (t) => {
+    const encoded = lnpayreq.encode(f.data)
 
-    let signedData = lnpayreq.sign(encoded, fixtures.privateKey)
+    const signedData = lnpayreq.sign(encoded, fixtures.privateKey)
 
     t.same(signedData.complete, true)
 
@@ -101,7 +101,7 @@ fixtures.encode.valid.forEach((f) => {
 })
 
 fixtures.encode.invalid.forEach((f) => {
-  tape(`test invalid vectors for encode`, (t) => {
+  tape('test invalid vectors for encode', (t) => {
     t.plan(1)
 
     t.throws(() => {
@@ -111,8 +111,8 @@ fixtures.encode.invalid.forEach((f) => {
 })
 
 fixtures.decode.valid.forEach((f) => {
-  tape(`test valid vectors for decode`, (t) => {
-    let decoded = lnpayreq.decode(f.paymentRequest, f.network)
+  tape('test valid vectors for decode', (t) => {
+    const decoded = lnpayreq.decode(f.paymentRequest, f.network)
 
     if (f.network === undefined) f.network = decoded.network
 
@@ -127,26 +127,26 @@ fixtures.decode.valid.forEach((f) => {
     t.end()
   })
 
-  tape(`test valid decode reverse encode without privateKey then with privateKey`, (t) => {
-    let decoded = lnpayreq.decode(f.paymentRequest, f.network)
-    let encodedNoPriv = lnpayreq.encode(decoded)
+  tape('test valid decode reverse encode without privateKey then with privateKey', (t) => {
+    const decoded = lnpayreq.decode(f.paymentRequest, f.network)
+    const encodedNoPriv = lnpayreq.encode(decoded)
 
-    delete decoded['signature']
-    delete decoded['recoveryFlag']
+    delete decoded.signature
+    delete decoded.recoveryFlag
 
-    let encodedWithPrivObj = lnpayreq.encode(decoded, false)
+    const encodedWithPrivObj = lnpayreq.encode(decoded, false)
 
-    delete encodedWithPrivObj['payeeNodeKey']
+    delete encodedWithPrivObj.payeeNodeKey
 
-    let signedData = lnpayreq.sign(encodedWithPrivObj, fixtures.privateKey)
+    const signedData = lnpayreq.sign(encodedWithPrivObj, fixtures.privateKey)
 
-    let encodedSignedData = lnpayreq.encode(signedData, false)
+    const encodedSignedData = lnpayreq.encode(signedData, false)
 
     encodedWithPrivObj.payeeNodeKey = signedData.payeeNodeKey
 
-    let signedData2 = lnpayreq.sign(encodedWithPrivObj, fixtures.privateKey)
+    const signedData2 = lnpayreq.sign(encodedWithPrivObj, fixtures.privateKey)
 
-    let signedData3 = lnpayreq.sign(signedData2, fixtures.privateKey)
+    const signedData3 = lnpayreq.sign(signedData2, fixtures.privateKey)
 
     t.same(f, encodedNoPriv)
     t.same(f, signedData)
@@ -159,7 +159,7 @@ fixtures.decode.valid.forEach((f) => {
 })
 
 fixtures.decode.invalid.forEach((f) => {
-  tape(`test invalid vectors for decode`, (t) => {
+  tape('test invalid vectors for decode', (t) => {
     t.plan(1)
 
     t.throws(() => {
@@ -171,8 +171,8 @@ fixtures.decode.invalid.forEach((f) => {
 // edge cases
 
 function tagsItems (tags, tagName) {
-  let tag = tags.filter(item => item.tagName === tagName)
-  let data = tag.length > 0 ? tag[0].data : null
+  const tag = tags.filter(item => item.tagName === tagName)
+  const data = tag.length > 0 ? tag[0].data : null
   return data
 }
 
@@ -180,7 +180,7 @@ function tagsContainItem (tags, tagName) {
   return tagsItems(tags, tagName) !== null
 }
 
-tape(`decode detects invalid network`, (t) => {
+tape('decode detects invalid network', (t) => {
   const f = fixtures.decode.valid[3]
   t.throws(() => {
     lnpayreq.decode(f.paymentRequest, { bech32: 'bc' })
@@ -188,8 +188,8 @@ tape(`decode detects invalid network`, (t) => {
   t.end()
 })
 
-tape(`encode adds defaults by default`, (t) => {
-  let encoded = lnpayreq.encode({
+tape('encode adds defaults by default', (t) => {
+  const encoded = lnpayreq.encode({
     tags: [
       {
         tagName: 'payment_hash',
@@ -207,8 +207,8 @@ tape(`encode adds defaults by default`, (t) => {
   t.end()
 })
 
-tape(`can decode upper case payment request`, (t) => {
-  let decoded = lnpayreq.decode('LNBC2500U1PVJLUEZPP5QQQSYQCYQ5RQWZQFQQQSYQC' +
+tape('can decode upper case payment request', (t) => {
+  const decoded = lnpayreq.decode('LNBC2500U1PVJLUEZPP5QQQSYQCYQ5RQWZQFQQQSYQC' +
                                 'YQ5RQWZQFQQQSYQCYQ5RQWZQFQYPQDQ5XYSXXATSYP3' +
                                 'K7ENXV4JSXQZPUAZTRNWNGZN3KDZW5HYDLZF03QDGM2' +
                                 'HDQ27CQV3AGM2AWHZ5SE903VRUATFHQ77W3LS4EVS3C' +
@@ -217,7 +217,7 @@ tape(`can decode upper case payment request`, (t) => {
   t.end()
 })
 
-tape(`can decode and encode payment request containing unknown tags`, (t) => {
+tape('can decode and encode payment request containing unknown tags', (t) => {
   const paymentRequest = 'lntb30m1pw2f2yspp5s59w4a0kjecw3zyexm7zur8l8' +
                          'n4scw674w8sftjhwec33km882gsdpa2pshjmt9de6zq' +
                          'un9w96k2um5ypmkjargypkh2mr5d9cxzun5ypeh2urs' +
@@ -253,14 +253,14 @@ tape(`can decode and encode payment request containing unknown tags`, (t) => {
   t.end()
 })
 
-tape(`can decode unknown network payment request`, (t) => {
+tape('can decode unknown network payment request', (t) => {
   const network = {
     bech32: 'sb',
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     validWitnessVersions: [0]
   }
-  let decoded = lnpayreq.decode(
+  const decoded = lnpayreq.decode(
     'lnsb1u1pwslkj8pp52u27w39645j24a0zfxnwytshxserjchdqt8nz8uwv9fp8wasxrhsdq' +
     'l2pkxz7tfdenjqum0w4hxggrgv4kxj7qcqzpgnvqq8t63nxmgha5945s633fdd3p5x9k889' +
     'g6p02qsghx4vrgqgr3xzz3hgld8r84ellwgz3teexvqzwlxj7lgkhl8xh2p7dstq0fgsspa' +
